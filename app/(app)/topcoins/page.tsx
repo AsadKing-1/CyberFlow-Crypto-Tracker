d8b0a6ee882a1df7ffb1b7edd759dfbb40fff3f2
AsadK1ng <asadbekmaratov23@gmail.com>
2026-05-20 19:09:17 +0300

    Add top coins search controls

--- app/(app)/topcoins/page.tsx
+++ app/(app)/topcoins/page.tsx
@@ -1,4 +1,5 @@
 import CryptoCards from "@/features/CryptoCards/ui/CryptoCards"
+import SearchCoin from "@/features/SearchCoin/ui/SearchCoin"
 
 export default function TopCoinsPage() {
     return (
@@ -7,6 +8,9 @@ export default function TopCoinsPage() {
             <p className="text-gray-400 mt-2 text-[15px]">Global crypto market perfomance, real-time data, and volume metrics updated continuously</p>
             <div className="mt-8">
                 <CryptoCards />
+            </div>
+            <div className="mt-8">
+                <SearchCoin/>
             </div>
         </div>
     )
